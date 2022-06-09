99e4ba377212f4bbfa95c8a837004d005ceabbd2
adil tahiri <rajaadil1995@gmail.com>
2022-06-09 01:07:03 +0100

    add some api basic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoClient = require("mongodb").MongoClient;
 const port = process.env.NODE_ENV || 8888;
 // ========================
 // Middlewares
@@ -18,6 +19,21 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/api/countries", (req, res) => {
+  res.json(country);
+});
+
+app.get("/api/countries/:name", (request, response) => {
+  const name = Number(request.params.name);
+  const country = countries.find((co) => co.name === name);
+  if (country) {
+    response.status(200).json(country);
+  } else {
+    response.status(404).end();
+  }
+  response.json(countries);
 });
 // ========================
 // listen
